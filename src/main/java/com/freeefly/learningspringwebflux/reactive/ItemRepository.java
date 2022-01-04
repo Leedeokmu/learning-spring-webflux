a525be1a518b6847b942a2e1a1720eec9c28b6c0
freeefly <freeefly@krafton.com>
2022-01-04 13:21:09 +0900

    feat: add webflux test code

---
+++ src/main/java/com/freeefly/learningspringwebflux/reactive/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.freeefly.learningspringwebflux.reactive;
+
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+
+public interface ItemRepository extends ReactiveCrudRepository<Item, String> {
+
+}
--- src/main/java/com/freeefly/learningspringwebflux/reactive/LearningSpringWebfluxApplication.java
+++ src/main/java/com/freeefly/learningspringwebflux/reactive/LearningSpringWebfluxApplication.java
@@ -2,11 +2,18 @@ package com.freeefly.learningspringwebflux.reactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.TemplateEngine;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class LearningSpringWebfluxApplication {
 
     public static void main(String[] args) {
+//        BlockHound.builder() // <1>
+//                .allowBlockingCallsInside( //
+//                        TemplateEngine.class.getCanonicalName(), "process") // <2>
+//                .install(); // <3>
+
         SpringApplication.run(LearningSpringWebfluxApplication.class, args);
     }
 
