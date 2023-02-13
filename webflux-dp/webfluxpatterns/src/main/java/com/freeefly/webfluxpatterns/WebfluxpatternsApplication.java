34e8bf3d121efb7a2b4c7910630f0bd2090c4a89
leedeokmu <deolee@gmarket.com>
2023-02-13 14:54:26 +0900

    feat: add webflux-dp project

---
+++ webflux-dp/webfluxpatterns/src/main/java/com/freeefly/webfluxpatterns/WebfluxpatternsApplication.java
@@ -0,0 +1,13 @@
+package com.freeefly.webfluxpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxpatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxpatternsApplication.class, args);
+    }
+
+}
---
+++ webflux-dp/webfluxpatterns/src/test/java/com/freeefly/webfluxpatterns/WebfluxpatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freeefly.webfluxpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxpatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
